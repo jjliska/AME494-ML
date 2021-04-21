9bb8916db17e8d509b0f56d1692d144fe9cc65ab
Joshua Liska <>
2021-04-20 21:22:13 -0700

    handDetection push

--- handDetection.py
+++ handDetection.py
@@ -26,12 +26,9 @@ with mp_hands.Hands(
       print("Ignoring empty camera frame.")
       # If loading a video, use 'break' instead of 'continue'.
       continue
-
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-
-    #Setting the screen grab to 224x224
+    # Getting image 224x224 image before we flip color and screen
     fc = cv2.resize(image, (224,224))
-    #Recoloring image
+    # Inverting color of screen
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
     # To improve performance, optionally mark the image as not writeable to
     # pass by reference.
@@ -42,26 +39,23 @@ with mp_hands.Hands(
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
-      #Creating an array from the image
+      data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
       inp = np.reshape(fc,(1,224,224,3)).astype(np.float32)
       image_array = np.asarray(inp)
       normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
       data[0] = normalized_image_array
-
-      #Getting prediction data from the array
       prediction = model.predict(data)
       gest = gesture[np.argmax(prediction)]
-      print(gest)
-
-      #Find all hands in the scene, and find the base position of the palm to determine vertical and horizontal of the hand
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-        data_point = hand_landmarks.landmark[0]
-        x=data_point.x
-        y=data_point.y
-        z=data_point.z
-      print(str(x)+","+str(y)+","+str(z))
+      #Getting the first hand rendered in the screen
+      hand_landmark = results.multi_hand_landmarks[0]
+      #Drawing the first hand in the scene
+      mp_drawing.draw_landmarks(image, hand_landmark, mp_hands.HAND_CONNECTIONS)
+      data_point = hand_landmark.landmark[0]
+      #Getting positional data of the wrist
+      x=data_point.x
+      y=data_point.y
+      z=data_point.z
+      print(gest+" @ ("+str(x)+","+str(y)+","+str(z)+")")
     cv2.imshow('MediaPipe Hands', image)
     if cv2.waitKey(5) & 0xFF == 27:
       break
