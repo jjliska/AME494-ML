9978469178f693af17415054439ad5c6ad0f163c
Joshua Liska <>
2021-04-20 18:26:28 -0700

    working hand demo

--- FaceDetection.py
+++ FaceDetection.py
@@ -1,25 +1,34 @@
+import tensorflow.keras
+from PIL import Image, ImageOps
+import numpy as np
 import cv2
 
-# Load the cascade
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
 
-# To capture video from webcam.
-cap = cv2.VideoCapture(0)
-# To use a video file as input
-# cap = cv2.VideoCapture('filename.mp4')
-
+cam = cv2.VideoCapture(0)
+# Load the model
+model = tensorflow.keras.models.load_model('keras_model.h5')
 while True:
     # Read the frame
-    _, img = cap.read()
-    # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Detect the faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    # Draw the rectangle around each face
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    ret, frame = cam.read()
+
+    fc = cv2.resize(frame, (224,224))
+    inp = np.reshape(fc,(1,224,224,3)).astype(np.float32)
+    image_array = np.asarray(inp)
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+    prediction = model.predict(data)
+
+    # display the resized image
+    cv2.imshow('frame', frame)
+
+    print(prediction)
+    #for (x, y, w, h) in faces:
+    #    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
     # Display
-    cv2.imshow('img', img)
+    #cv2.imshow('img', img)
     # Stop if escape key is pressed
     k = cv2.waitKey(30) & 0xff
     if k==27:
